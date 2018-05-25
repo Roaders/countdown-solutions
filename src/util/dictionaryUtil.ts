51e050bfc2df013331fad903ff2338bbbe6a243f
unknown <giles@roadnight.name>
2018-05-25 20:33:01 +0100

    implemented word groups

--- src/util/dictionaryUtil.ts
+++ src/util/dictionaryUtil.ts
@@ -2,22 +2,40 @@
 import * as fs from "fs";
 
 const maxLength = 12;
+const minLength = 3;
 
 type wordGroup = {
-    maxLength: number;
-    
+    length: number;
+    words: string[];
 };
 
 export function createWordLists(path: string){
 
-    const words = fs.readFileSync(path).toString().split("\r\n");
+    const words = fs.readFileSync(path).toString().split(/\r?\n/);
 
-    const maxLength = words.reduce<number>((currentMax,word) => Math.max(currentMax, word.length), 0);
+    console.log(`Words: ${words.length}`);
 
+    const groups = words.reduce<wordGroup[]>((groups,word) => addWordToGroups(word, groups), []);
 
-    console.log(`Max length: ${maxLength}`);
+    console.log(`Groups: ${groups.map(g => g.length + ":" + g.words.length).join(", ")}`);
 }
 
-function addWordToGroups(){
+function addWordToGroups(word: string, groups: wordGroup[]){
+    if(word.length > maxLength || word.length < minLength){
+        return groups;
+    }
 
+    let group: wordGroup = groups.filter(g => g.length === word.length)[0];
+    if(group == null){
+        group = {
+            length: word.length,
+            words: []
+        }
+
+        groups.push(group);
+    }
+
+    group.words.push(word);
+
+    return groups;
 }
